8681046a50cd4ce0e3a826f1b3817ceedf64b1f7
nirine1 <ilainiriko.tambaza@gmail.com>
2026-07-12 16:17:13 +0200

    chore: trust reverse proxy headers for TLS-terminating host

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state', 'locale']);
 
         $middleware->web(append: [
